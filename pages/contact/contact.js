b4ba1d1ac1a0ee9d1613c88cdd81159af1a90eee
Kveta Mooney <132582410+selfcoder-gazell@users.noreply.github.com>
2024-02-25 17:49:43 +0000

    starting to work on my CV page

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Script from "next/script";
 import Layout from "../../components/layout";
 
-export default function About() {
+export default function Contact() {
     return (
         <Layout>
         
--- pages/cv/cv.js
+++ pages/cv/cv.js
@@ -2,14 +2,16 @@ import Link from "next/link";
 import Head from "next/head";
 import Script from "next/script";
 import Layout from "../../components/layout";
+import React from "react";
 
-export default function About() {
+export default function CV() {
     return (
         <Layout>
         
         <Head>
             <title>CV</title>
         </Head>
+        
         <h2>
               CV
             </h2>
