acff847b6c28935c592dda08be38f928d50e042c
Kveta Mooney <132582410+selfcoder-gazell@users.noreply.github.com>
2024-02-29 15:08:41 +0000

    content of CV progression

--- pages/cv/cv.js
+++ pages/cv/cv.js
@@ -6,7 +6,7 @@ import React from "react";
 import styles from "./cv.module.css";
 import utilStyles from '/styles/utils.module.css';
 import Image from "next/image";
-
+import MyDocument from "../_document";
 
 export default function CV() {
     return (
@@ -16,10 +16,11 @@ export default function CV() {
             {/* no need to add viewport meta tag as Next.js automatically adds it into the <Head> section */}
             <title>CV - resume</title>
         </Head>
+        
         <div className={styles.resumeCanvas}>
         <div className={styles.gridParent}>
 
-<div className={styles.div1}> 
+<div className={styles.topHeading}> 
 <h3 className={styles.heading}>KVETA MOONEY</h3>
     
         <Image
@@ -30,15 +31,50 @@ export default function CV() {
         width={99}
         alt="profileimage"
         />
+<span>
+    <h5 className={styles.h5}>SOFTWARE DEVELOPER</h5>
+    </span>
 
-<h5 className={styles.h5}>SOFTWARE DEVELOPER</h5>
     
 </div>
 
 
-<div className={styles.div2}> </div> 
-<div className={styles.div3}> 
+<div className={styles.leftColumn}>
+    <div className={styles.leftContainer}>
 
+    <h6 className={styles.centeredText}>EXPERIENCE</h6> 
+    <p>Full time Bootcamper at School of Code</p>
+    <p>(Sep.2023-Present)</p>
+    <p className={styles.text}>We were selected from over 3000 candidates to join this 90-person, full-time, fully remote, 16-weeks fast-paced web development boot camp. We delved straight into full-stack modern and relevant technologies. I've learned how to collaborate in pair programming in a team following agile methodologies, along with exciting technical skills and many invaluable soft skills.</p>
+    <p>Final Project: Harvest Hub</p>
+    <p className={styles.text}>Harvest Hub is the ultimate web application designed to nurture our passion for all things green. The App helps people find information about how to grow vegetable and track their growth and expected harves date. </p>
+    </div> 
+    </div>
+
+<div className={styles.rightColumn}> 
+<div className={styles.rightContainer}>
+<h6 className={styles.centeredText}>TOOLS & TECHNOLOGIES</h6> 
+<ul className={styles.text}>
+    <li>Java Scrip</li>
+    <li>HTML5</li>
+    <li>CSS3</li>
+    <li>React</li>
+    <li>Git / GitHub </li>
+    <li>PostgreSQL</li>
+    <li>Figrma</li>
+    <li>Slack</li>
+    <li>VS Code</li>
+</ul>
+<h6 className={styles.centeredText}>INDUSTRY KNOWLEDGE</h6> 
+<ul className={styles.text}>
+    <li>UI/UX Design</li>
+    <li>Disney Ideation</li>
+    <li>User stories and personas</li>
+    <li>Wireframe Creation</li>
+    <li>Pair Programming</li>
+    <li>Scrum / Kanban </li>
+</ul>
+</div>
 </div>
 
 
