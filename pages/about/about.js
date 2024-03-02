f4e1b794638d5dc3a29b02473c4922b66efb0113
Kveta Mooney <132582410+selfcoder-gazell@users.noreply.github.com>
2024-03-02 12:33:27 +0000

    about info page

--- pages/about/about.js
+++ pages/about/about.js
@@ -11,7 +11,26 @@ export default function About() {
             <title>About me</title>
         </Head>
         <h2>
-              About
+              I never quite knew what i wanted to do as a job in my life. 
+              It was easier to work out what I did not want to do. 
+              I was gradually working out what parts of different jobs I enjoy. 
+              I dive into the world of web development out of blue. 
+              Friend of mine suggestion came from nowhere(2022).
+              I discarded his/her idea immediatelly. 
+              My image of a web/software developer was (like many other's) wrong. 
+              I was imagining it as spending days/weeks without speaking to a soul in front of a screen(s).
+              That's SAD, right? LONELY... I am a social creature. 
+              I need to work with people i can call my second family.
+              I started to explore the field of web development little bit more and gues what?
+              I was wrong?? 
+              When I came across School of Code (free of charge) I could not belive my luck. 
+              My first attempt to be accepted into the School of Code - FAILED (Jan.2023). 
+              It is scary to change a career at any age and I felt excatly that.
+              Fear of change, faillure, shame ... exc. IMPOSTER SYNDROM written all over. 
+              I applied again and I got accepted (Sep.2023) - SUCCESS at last. 
+              Along with 90odd others (Out of more than 3000 applications).
+
+
             </h2>
         </Layout>
     
