1d8f396fe32bcf6bfdb3fb4fa4064db8ab051494
Kveta Mooney <132582410+selfcoder-gazell@users.noreply.github.com>
2024-03-02 18:59:43 +0000

    about content

--- pages/about/about.js
+++ pages/about/about.js
@@ -11,25 +11,37 @@ export default function About() {
             <title>About me</title>
         </Head>
         <h5>
-              I dive into the world of web development out of blue. 
-              Friend of mine suggestion came from nowhere (2022).
-              I discarded his idea immediatelly. 
-              My image of a web/software developer was (like many other's) wrong. 
-              I was imagining it as spending days/weeks without speaking to a soul in front of a screen(s).
-              That's SAD, right? LONELY... I am a social creature. 
-              I need to work with people I can call my second family.
-              I started to explore the field of web development little bit more and gues what?
-              I was wrong?? 
-              When I came across School of Code (free of charge) I could not belive my luck. 
-              My first attempt to be accepted into the School of Code - FAILED (Jan.2023). 
+              <p>I dived into the world of web development out of blue. 
+              A friend suggested for me to take the leap in 2022.
+              I discarded his idea immediatelly. </p>
+              <br></br>
+              <p>
+              My idea of software developer was mainly about spending days/weeks without speaking to a soul in front of a screen(s).
+              That's SAD, right? LONELY...</p>
+              <p>
+              <br></br>
+              <p>
+              I started to explore the field of web development little bit more and guess what?
+              I was so wrong!! 
+              </p>
+                <br></br>
+              I am a social creature. 
+              I need to work with people I could call my second family.
+              </p>
+         <br></br>
+         <p>When I came across School of Code (FREE of charge) I could not belive my luck. 
+              My attempt to be accepted into the School of Code - FAILED (Jan.2023). 
               It is scary to change a career at any age and I felt excatly that.
-              Fear of change, faillure, shame ... exc. IMPOSTER SYNDROM written all over. 
+              Fear of change, faillure, shame ... exc. IMPOSTER SYNDROM written all over.</p>
+              <br></br>
               I applied again and I got accepted (Sep.2023) - SUCCESS at last. 
-              Along with 90odd others (Out of more than 3000 applications).
-              It felt as i was being a traveler in a foreign country without a map. 
+              Along with 90odd others (Out of more than 3000 applications) we delved straght into full-stack technologies.
+              
+              <p><br></br>It felt as i was being a traveler in a foreign country without a map. 
               Feelings of disorientation at first, but as I explored and learned, 
               I started to recognize landmarsk, understand the language, and find my way around. 
-              We dive straight into 
+                </p>
+                <p>I am still finding my way now .... but now I got a MAP :-)  </p>
 
 
             </h5>
