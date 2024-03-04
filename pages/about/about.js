9a0319ccdebe53c2c04d9afe92c7688ac521eae1
Kveta Mooney <132582410+selfcoder-gazell@users.noreply.github.com>
2024-03-04 11:08:01 +0000

    about page content updated

--- pages/about/about.js
+++ pages/about/about.js
@@ -12,26 +12,29 @@ export default function About() {
         </Head>
         <h5>
               <p>Diving headfirst into the world of software development was a decision born out of curiosity and a nudge from a friend back in 2022.
-              I dismissed the idea, imagining a solitary existence glued to screens for endless hours - a prospect that felt isolating and disheartening.</p>
+              I dismissed the idea, imagining a solitary existence glued to screens for endless hours - a prospect that felt isolating and disheartening.
+              </p>
               <br></br>
               <p>
-              But as I delved deeper intot he field, I discovered that I was wrong, soo.. wrong!
-              As a social creature, I craved connections and collaboration, seeking a work enviroment similar to a second family.
+              But as I delved deeper into  he field, I discovered that I was wrong, soo.. wrong!
+              As a social creature, I craved connections and collaboration, seeking a work environment similar to a second family.
               </p>
          <br></br>
-         <p>When I came across School of Code (FREE of charge) I could not belive my luck. 
-              My attempt to be accepted into the School of Code - FAILED (Jan.2023). 
-              It is scary to change a career at any age and I felt excatly that.
-              Fear of change, faillure, shame ... exc. IMPOSTER SYNDROM written all over.</p>
+         <p>I have been lost in the ocean of information and the amount of coding languages I could choose from. 
+            When I came across School of Code I could not believe my luck. 
+              My attempt to be accepted into the School of Code in January 2023 FAILED. 
+              It is scary to change a career at any age and I felt exactly that.
+              Fear of change, failure, shame ... ect. IMPOSTER SYNDROM written all over.</p>
               <br></br>
-              I applied again and I got accepted (Sep.2023) - SUCCESS at last. 
-              Along with 90odd others (Out of more than 3000 applications) we delved straght into full-stack technologies.
+              I applied again and I got accepted in September 2023 - SUCCESS at last. 
+              Along with 90 like-minded individuals selected from a pool of over 3000 applications, 
+              we delved straight into full-stack technologies.
               
-              <p><br></br>It felt as i was being a traveler in a foreign country without a map. 
+              <p><br></br>It felt as i was being a traveller in a foreign country without a map. 
               Feelings of disorientation at first, but as I explored and learned, 
-              I started to recognize landmarsk, understand the language, and find my way around. 
+              I started to recognize landmarks, understand the language, and find my way around. 
                 </p>
-                <p>I am still finding my way now .... but now I got a MAP :-)  </p>
+                <p>I am still finding my way .... but now I got a MAP :-)  </p>
 
 
             </h5>
