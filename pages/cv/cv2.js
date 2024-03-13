d7d6e9dc229b63efc68379fb340564d9697d54de
Kveta Mooney <132582410+selfcoder-gazell@users.noreply.github.com>
2024-03-13 13:17:45 +0000

    experimental CV2page progress

--- pages/cv/cv2.js
+++ pages/cv/cv2.js
@@ -14,13 +14,18 @@ export default function CV2() {
             {/* no need to add viewport meta tag as Next.js automatically adds it into the <Head> section */}
             <title>CV - resume</title>
         </Head>
-        <div>
+        <div style={{ height: '100vh', overflow:'auto' }}>
+
+        
             <h1>My CV</h1>
-            <object type="image/svg+xml" data="/cv1.svg" width="100%" height="100%">
-                Your browser does not support SVG
-            </object>
-            <img src="/cv1.svg" alt="CV" />
-            <img src="/cv2.svg" alt="CV" />
+            <iframe src="/image/cv.pdf" style={{ width: '100%', height: '100%', border: 'none' }}></iframe>
+            
+
+        <div>
+            
+            <a href="/image/cv.pdf" download>Download my CV PDF</a>
+        </div>
+            
         </div>
 
 </Layout>
