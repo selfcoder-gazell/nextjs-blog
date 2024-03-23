e21393e1d204084f37c273b36e9007e246c0455e
Kveta Mooney <132582410+selfcoder-gazell@users.noreply.github.com>
2024-03-23 16:57:31 +0000

    new CV available for download

--- pages/cv/cv.js
+++ pages/cv/cv.js
@@ -7,6 +7,12 @@ import utilStyles from '/styles/utils.module.css';
 import Image from "next/image";
 import styles from './cv.module.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import {
+  faIdBadge,
+} from "@fortawesome/free-solid-svg-icons";
+
 export default function CV() {
     return (
         <Layout>
@@ -20,15 +26,40 @@ export default function CV() {
 
         
             
-            <iframe src="/image/cv.pdf" style={{ width: '100%', height: '98rem', border: 'none' }}></iframe>
-            
+            <iframe src="/image/cv.pdf" style={{ width: '100%', height: '100rem', border: 'none' }}></iframe>
 
-        <div className={styles.centeredText}> 
+            <Link href="/image/cv.pdf">
+            <svg 
+            className={styles.hexagonIcon}
+            viewBox="0 0 173.20508075688772 200" 
+            height="200" 
+            width="174" 
+            version="1.1" 
+            xmlns="http://www.w3.org/2000/svg">
+              <path 
+              d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" 
+              
+              
+              fill="#1e2530"></path>
         
+
+              <FontAwesomeIcon
+               icon={faIdBadge}
+               x="60"
+                y="70"
+              width="50"
+              height="50"
+              color="gold"
+               />
+
+              <text 
+              className={styles.title}
+              x="50%" 
+              y="69%" 
+              dominant-baseline="middle" 
+              text-anchor="middle" 
+              fill="white">CV .pdf download</text></svg></Link>
         
-            
-            <a href="/image/cv.pdf" download>Download/Open this CV in PDF</a>
-        </div>
             
         </div>
 
