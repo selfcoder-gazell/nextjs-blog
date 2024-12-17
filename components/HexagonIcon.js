7389af13601807fb5b83524aacc3b0bbc4d60ef5
FoxMoo-me <kvetamooney@gmail.com>
2024-12-17 14:13:44 +0000

    styling of nav link icons

--- components/HexagonIcon.js
+++ components/HexagonIcon.js
@@ -63,6 +63,7 @@
 
 // export default HexagonIcon;
 
+
 import React from 'react';
 import Link from 'next/link';
 import OuterHexagon from './OuterHexagon';
@@ -70,36 +71,117 @@ import InnerHexagon from './InnerHexagon';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './HexagonIcon.module.css';
 
-const HexagonIcon = ({ outerFill, innerFill, label, icon, link }) => {
+const HexagonIcon = ({ outerFill, innerFill, label, icon, link, isActive }) => {
+  console.log('Is Active:', label, isActive); // Debugging active state
+
   return (
-    <Link href={link}>
-      <OuterHexagon fill={outerFill}>
-        <InnerHexagon fill={innerFill} />
-        <FontAwesomeIcon
-          icon={icon}
-          className={styles.icon}
-          width={50}
-          height={50}
-          x="49"
-          y="60"
-          // style={{ position: 'absolute', top: '49px', left: '60px' }}
-        />
-        <text
-          className={styles.title}
-          x="50%"
-          y="70%"
-          dominantBaseline="middle"
-          textAnchor="middle"
-          fill="white"
-        >
-          {label}
-        </text>
-      </OuterHexagon>
+    <Link href={link} aria-current={isActive ? 'page' : undefined}>
+      <div
+        className={`${styles.hexagonIcon} ${isActive ? styles.active : ''}`}
+        style={{ position: 'relative' }}
+      >
+        <OuterHexagon fill={outerFill}>
+          <InnerHexagon fill={innerFill} />
+          <FontAwesomeIcon
+            icon={icon}
+            className={styles.icon}
+            width={50}
+             height={50}
+             x="75"
+             y="60"
+            style={{ position: 'absolute', top: '60px', left: '62px' }}
+          />
+          <text
+            className={styles.title}
+            x="50%"
+            y="70%"
+            dominantBaseline="middle"
+            textAnchor="middle"
+            fill="white"
+          >
+            {label}
+          </text>
+        </OuterHexagon>
+      </div>
     </Link>
   );
 };
 
 export default HexagonIcon;
+
+// const HexagonIcon = ({ outerFill, innerFill, label, icon, link, isActive }) => {
+//   return (
+//     <Link href={link} aria-current={isActive ? 'page' : undefined}>
+//       <div
+//         className={`${styles.hexagonIcon} ${isActive ? styles.active : ''}`}
+//         style={{ position: 'relative' }}
+//       >
+//         <OuterHexagon fill={outerFill}>
+//           <InnerHexagon fill={innerFill} />
+//           <FontAwesomeIcon
+//             icon={icon}
+//             className={styles.icon}
+//             width={50}
+//             height={50}
+//             x="75"
+//             y="60"
+//             style={{ position: 'absolute', top: '60px', left: '62px' }}
+//           />
+//           <text
+//             className={styles.title}
+//             x="50%"
+//             y="70%"
+//             dominantBaseline="middle"
+//             textAnchor="middle"
+//             fill="white"
+//           >
+//             {label}
+//           </text>
+//         </OuterHexagon>
+//       </div>
+//     </Link>
+//   );
+// };
+
+// export default HexagonIcon;
+
+// import React from 'react';
+// import Link from 'next/link';
+// import OuterHexagon from './OuterHexagon';
+// import InnerHexagon from './InnerHexagon';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import styles from './HexagonIcon.module.css';
+
+// const HexagonIcon = ({ outerFill, innerFill, label, icon, link }) => {
+//   return (
+//     <Link href={link}>
+//       <OuterHexagon fill={outerFill}>
+//         <InnerHexagon fill={innerFill} />
+//         <FontAwesomeIcon
+//           icon={icon}
+//           className={styles.icon}
+//           width={50}
+//           height={50}
+//           x="49"
+//           y="60"
+//           // style={{ position: 'absolute', top: '49px', left: '60px' }}
+//         />
+//         <text
+//           className={styles.title}
+//           x="50%"
+//           y="70%"
+//           dominantBaseline="middle"
+//           textAnchor="middle"
+//           fill="white"
+//         >
+//           {label}
+//         </text>
+//       </OuterHexagon>
+//     </Link>
+//   );
+// };
+
+// export default HexagonIcon;
 
 
 // import React from 'react';
--- components/Navbar.js
+++ components/Navbar.js
@@ -27,8 +27,50 @@
 // };
 
 // export default Navbar;
+// import Link from 'next/link';
+// import React from 'react';
+// import HexagonIcon from './HexagonIcon';
+// import {
+//   faUniversalAccess,
+//   faBullseye,
+//   faIdBadge,
+//   faLifeRing,
+//   faMapSigns,
+//   faBook,
+// } from '@fortawesome/free-solid-svg-icons';
+// import styles from './Navbar.module.css';
+
+// const navItems = [
+//   { label: 'HOME', icon: faUniversalAccess, outerFill: '#3498db', innerFill: '#2980b9', link: '/' },
+//   { label: 'ABOUT', icon: faBullseye, outerFill: '#e74c3c', innerFill: '#c0392b', link: '/about/about' },
+//   { label: 'PROJECTS', icon: faIdBadge, outerFill: '#2ecc71', innerFill: '#27ae60', link: '/projects/projects' },
+//   { label: 'CV', icon: faLifeRing, outerFill: '#f1c40f', innerFill: '#f39c12', link: '/cv/cv' },
+//   { label: 'CONTACT', icon: faMapSigns, outerFill: '#44474d', innerFill: '#44474d', link: '/contact/contact' },
+//   { label: 'BLOG', icon: faBook, outerFill: '#44474d', innerFill: '#44474d', link: '/blog/blog' },
+// ];
+
+// const Navbar = () => {
+//   return (
+//     <div className={styles.navbar}>
+//       {navItems.map((item, index) => (
+//         <HexagonIcon
+//           key={index}
+//           label={item.label}
+//           icon={item.icon}
+//           outerFill={item.outerFill}
+//           innerFill={item.innerFill}
+//           link={item.link}
+//         />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
 import Link from 'next/link';
 import React from 'react';
+import { useRouter } from 'next/router';
 import HexagonIcon from './HexagonIcon';
 import {
   faUniversalAccess,
@@ -50,23 +92,54 @@ const navItems = [
 ];
 
 const Navbar = () => {
+  const router = useRouter();
+
+  console.log('Current Path:', router.pathname);
+
   return (
     <div className={styles.navbar}>
-      {navItems.map((item, index) => (
-        <HexagonIcon
-          key={index}
-          label={item.label}
-          icon={item.icon}
-          outerFill={item.outerFill}
-          innerFill={item.innerFill}
-          link={item.link}
-        />
-      ))}
+      {navItems.map((item, index) => {
+        console.log('Nav Item Link:', item.link);
+        return (
+          <HexagonIcon
+            key={index}
+            label={item.label}
+            icon={item.icon}
+            outerFill={item.outerFill}
+            innerFill={item.innerFill}
+            link={item.link}
+            isActive={router.pathname === item.link} // Check if this is true
+          />
+        );
+      })}
     </div>
   );
 };
 
 export default Navbar;
+
+// const Navbar = () => {
+//   const router = useRouter();
+//   console.log('Current Path:', router.pathname);
+
+//   return (
+//     <div className={styles.navbar}>
+//       {navItems.map((item, index) => (
+//         <HexagonIcon
+//           key={index}
+//           label={item.label}
+//           icon={item.icon}
+//           outerFill={item.outerFill}
+//           innerFill={item.innerFill}
+//           link={item.link}
+//           isActive={router.pathname === item.link} // Pass isActive prop
+//         />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Navbar;
 
 
 
